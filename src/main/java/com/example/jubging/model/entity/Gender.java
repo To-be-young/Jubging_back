246add2ecd591ec8b429f1c1beeedbdddafa35b1
kong <kongji4092@gmail.com>
2022-07-18 20:16:42 +0900

    Jwt 발급 및 인증 클래스 작성

---
+++ src/main/java/com/example/jubging/model/entity/Gender.java
@@ -0,0 +1,5 @@
+package com.example.jubging.model.entity;
+
+public enum Gender {
+    MALE, FEMALE, NONE
+}
---
+++ src/main/java/com/example/jubging/model/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.jubging.model.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 필수 입력
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "gender")
+    private Gender gender;
+
+
+    // TODO
+    // 토큰
+    // 누적 플로깅 거리, 횟수 정보
+}
