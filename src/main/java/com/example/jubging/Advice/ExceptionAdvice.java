6f4ef239420311aac2b0abf62214a338a0eeb80e
kong <kongji4092@gmail.com>
2022-08-24 16:39:09 +0900

    api 예외처리 메시지 추가

--- src/main/java/com/example/jubging/Advice/ExceptionAdvice.java
+++ src/main/java/com/example/jubging/Advice/ExceptionAdvice.java
@@ -5,13 +5,10 @@ import com.example.jubging.Service.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLIntegrityConstraintViolationException;
 
 @RequiredArgsConstructor
 @RestControllerAdvice
@@ -21,10 +18,26 @@ public class ExceptionAdvice {
     private final ResponseService responseService;
     private final MessageSource messageSource;
 
-//    회원가입 중복 아이디 가입시 발생시키는 예외
-    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public CommonResult singUpOverlapUserId(Exception e){
-        log.info("[회원가입 아이디 중복 오류 발생]");
-        return responseService.getFailResult(500, "회원가입 중복 오류 발생");
+//     기본 오류
+    @ExceptionHandler(Exception.class)
+    public CommonResult defaultException(Exception e){
+        log.info(String.valueOf(e));
+        return responseService.getFailResult(500, "Unknown error");
     }
+
+//    회원가입 중복 아이디 가입시 발생시키는 예외
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public CommonResult singUpDuplicateUserIdException(Exception e){
+        log.info("[회원가입 아이디 중복 오류 발생]");
+        return responseService.getFailResult(400, "[BAD REQUEST] 회원가입 중복 오류 발생");
+    }
+
+//    회원가입 no value
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public CommonResult signUpNoValueException(Exception e){
+        log.info("[회원가입시 필요한 값 없음]");
+        return responseService.getFailResult(400, "[BAD REQUEST] 필요한 값 확인해주세요");
+    }
+
+
 }
--- src/main/java/com/example/jubging/Controller/SignController.java
+++ src/main/java/com/example/jubging/Controller/SignController.java
@@ -4,10 +4,8 @@ import com.example.jubging.DTO.LoginDTO;
 import com.example.jubging.DTO.TokenDTO;
 import com.example.jubging.DTO.UserDTO;
 import com.example.jubging.Response.SingleResult;
-import com.example.jubging.Repository.UserRepository;
-import com.example.jubging.Service.RecordService;
-import com.example.jubging.Service.response.ResponseService;
 import com.example.jubging.Service.SignService;
+import com.example.jubging.Service.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,15 +13,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/sign")
 public class SignController {
-    private final UserRepository userRepository;
     private final SignService signService;
     private final ResponseService responseService;
-    private final RecordService recordService;
 
     //    TODO
     @PostMapping("/login")
@@ -34,7 +32,7 @@ public class SignController {
     }
 
     @PostMapping("/signup")
-    public SingleResult<String> signUp(@RequestBody UserDTO userDTO) {
+    public SingleResult<String> signUp(@RequestBody @Valid UserDTO userDTO) {
         log.info("[회원가입]");
         signService.signUp(userDTO);
         return responseService.getSingleResult(userDTO.getUserId());
