ed993105d68101f3cffd20f7cf8b8eae807ff5dc
Hyunsoo Kim <optifun72@gmail.com>
2022-10-02 21:28:13 +0900

    플로깅 참여, 플로깅 커뮤니티 닉네임 추가

--- src/main/java/com/example/jubging/Model/JoinMember.java
+++ src/main/java/com/example/jubging/Model/JoinMember.java
@@ -1,20 +1,25 @@
-//package com.example.jubging.Model;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-//@Builder
-//@Entity
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Getter
-//@Table(name = "joinMember")
-//public class JoinMember {
-//
-//}
+package com.example.jubging.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@IdClass(JoinMemberId.class)
+@Table(name = "joinMember")
+public class JoinMember {
+    @Id
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name="CommunityPost_PostId")
+    private CommunityPost postId;
+
+    @Id
+    private Long userId;
+}
